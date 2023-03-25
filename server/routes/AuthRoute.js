564599d0f2be0ea8db22d7492489ba91e731c26d
isiis0406 <Issabobobaldepro@gmail.com>
2023-03-25 18:28:12 +0100

    Api server : User Login done

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,35 +1,56 @@
 import express from "express";
-import {userModel} from "../models/user.js";
-import jwt  from "jsonwebtoken";
+import { userModel } from "../models/user.js";
+import jwt from "jsonwebtoken";
 import bcrypt from 'bcrypt';
 const router = express.Router();
 
 //Register
 router.post('/register', async (req, res) => {
 
-    const {email, password} = req.body;
-    const user = await userModel.findOne({email});
+    const { email, password } = req.body;
 
-    //Check points
-    //Does user exists
-    if(user){
-        return res.status(404).json({message: "Utilisateur déjà existant!"});
-    }
-   
+
     try {
-         const hashedPassword = await bcrypt.hash(password, 10);
-        await userModel.create({email, password: hashedPassword});
-        res.status(201).json({message: "Utilisateur enregistré avec succès"});
+        const user = await userModel.findOne({ email });
+        //Check points
+        //Does user exists
+        if (user) {
+            return res.status(404).json({ message: "Utilisateur déjà existant!" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await userModel.create({ email, password: hashedPassword });
+        res.status(201).json({ message: "Utilisateur enregistré avec succès" });
     } catch (error) {
-        res.status(505).json({message: error.message});
+        res.status(505).json({ message: error.message });
     }
 
 })
 
 //Login
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     //
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    //Check points
+
+    try {
+        //Does user exists
+        //Does user exists
+        if (!user) {
+            return res.status(404).json({ message: "L'utilisateur n'existe pas!" });
+        }
+        //Does password valid
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!isValidPassword) {
+            return res.status(404).json({ message: "Email ou mot de passe incorrect!" })
+        }
+        const token = jwt.sign({ id: user._id }, process.env.SECRET);
+        res.status(200).json({ token, userID: user._id });
+
+    } catch (error) {
+        res.status(505).json({ message: error.message });
+    }
 })
 
 
-export { router as authRouter};
+export { router as authRouter };
